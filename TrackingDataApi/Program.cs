317ad6ed79fc8181b89fbc0f10ecc0efe2c7018d
a.hamidi <a.hamidi@stsepehr.ir>
2023-03-05 14:38:43 +0330

    remove sw agger

--- TrackingDataApi/Program.cs
+++ TrackingDataApi/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
